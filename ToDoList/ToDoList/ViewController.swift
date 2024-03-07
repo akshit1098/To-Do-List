4b180864a62444d3de078e32a10b8fd7c9d0b3a6
akshit1098 <saxena.aks@northeastern.edu>
2024-03-07 15:17:43 -0500

    Opening Task Details functionality added

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -79,7 +79,18 @@ extension ViewController: UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = storyboard?.instantiateViewController(withIdentifier: "task") as! TaskViewController
+        vc.title = "New Task"
+        vc.task = tasks[indexPath.row]
+        
+        navigationController?.pushViewController(vc, animated: true)
+        
+        
+        
     }
+    
+    
     
 }
 
